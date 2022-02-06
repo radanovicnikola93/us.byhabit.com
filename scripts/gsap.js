e1bd16c5a7664835b29e0c3968a98a318c834b22
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-06 13:40:42 +0100

    structured js files in a folder and modified minor changes for js

---
+++ scripts/gsap.js
@@ -0,0 +1,5 @@
+// GSAP 
+
+// ------------------------- HEADER -------------------------
+gsap.from('.page-header', {opacity: 0, duration: 0.7, scale: 1.2}) // header
+gsap.from('.slogan', {opacity: 0, duration: 1, y: -50, delay: 0.8}) // header
---
+++ scripts/simple-parallax.js
@@ -0,0 +1,4 @@
+import SimpleParallax from "simple-parallax-js";
+
+let image = document.querySelectorAll('img');
+new SimpleParallax(image)
