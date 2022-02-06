8d36b732c353781e6a38cfd5fcd5b2032917118a
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-06 13:26:08 +0100

    added gsap animation for the header

--- app.js
+++ app.js
@@ -7,5 +7,8 @@
 // }
 // Scrollbar.init(document.querySelector('#my-scrollbar'), options);
 
-// GSAP
-gsap.from('.page-header', {opacity: 0, duration: 1, y: -50}) // header
+// GSAP 
+
+// ------------------------- HEADER -------------------------
+gsap.from('.page-header', {opacity: 0, duration: 0.7, scale: 1.2}) // header
+gsap.from('.slogan', {opacity: 0, duration: 1, y: -50, delay: 0.8}) // header
