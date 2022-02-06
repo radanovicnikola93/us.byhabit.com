a020a65ed22ef29a45ff182a1b277e9cd0dc203c
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-06 15:36:21 +0100

    added scroll-trigger for product-gallery

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -5,7 +5,7 @@ gsap.from('#page-header', {opacity: 0, duration: 0.7, scale: 1.2}) // header
 gsap.from('#page-header-slogan', {opacity: 0, duration: 1, y: -50, delay: 0.8}) // header
 
 // ------------------------- GALLERY SECTION -------------------------
-const timeline = (target, trigger) => {
+const timeline = (target, trigger, x, y, opacity, duration) => {
     let tl = gsap.timeline({ // generating a timeline
         scrollTrigger: {
             trigger: trigger,
@@ -14,16 +14,30 @@ const timeline = (target, trigger) => {
     });
 
     tl.from(target, {
-        y: 50, 
-        opacity: 0, 
-        duration: 1.8
+        x: x,
+        y: y, 
+        opacity: opacity, 
+        duration: duration
     })
 }
 
-timeline("#content-text-1", "#content-text-1");
-timeline("#content-text-2", "#content-text-2");
-timeline("#content-text-3", "#content-text-3");
-timeline("#content-text-4", "#content-text-4");
+timeline("#content-text-1", "#content-text-1", 0, 40, 0, 2);
+timeline("#content-text-2", "#content-text-2", 0, 40, 0, 2);
+timeline("#content-text-3", "#content-text-3", 0, 40, 0, 2);
+timeline("#content-text-4", "#content-text-4", 0, 40, 0, 2);
 
 // ------------------------- GALLERY SECTION -------------------------
-timeline("#product-header", "#product-header-slogan");
+timeline("#product-header", "#product-header-slogan", 0, 40, 0, 2);
+
+// ------------------------- PRODUCT GALLERY -------------------------
+timeline("#product-gallery-main-product-1", "#product-gallery-main-product-1", 0, 0, 0, 2);
+timeline("#product-gallery-links-1", "#product-gallery-links-1", 0, 0, 0, 2);
+timeline("#product-gallery-shop-products-1", "#product-gallery-shop-products-1", 0, 0, 0, 2);
+
+timeline("#product-gallery-main-product-2", "#product-gallery-main-product-2", 0, 0, 0, 2);
+timeline("#product-gallery-links-2", "#product-gallery-links-2", 0, 0, 0, 2);
+timeline("#product-gallery-shop-products-2", "#product-gallery-shop-products-2", 0, 0, 0, 2);
+
+timeline("#product-gallery-main-product-3", "#product-gallery-main-product-3", 0, 0, 0, 2);
+timeline("#product-gallery-links-3", "#product-gallery-links-3", 0, 0, 0, 2);
+timeline("#product-gallery-shop-products-3", "#product-gallery-shop-products-3", 0, 0, 0, 2);
