8fcf27c59e2301c0a359f65a39e5e4d8032688d2
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-02-06 15:11:55 +0100

    added scroll-trigger for gallery section

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,5 +1,25 @@
 // GSAP 
-
+gsap.registerPlugin(ScrollTrigger);
 // ------------------------- HEADER -------------------------
-gsap.from('.page-header', {opacity: 0, duration: 0.7, scale: 1.2}) // header
-gsap.from('.slogan', {opacity: 0, duration: 1, y: -50, delay: 0.8}) // header
+gsap.from('#page-header', {opacity: 0, duration: 0.7, scale: 1.2}) // header
+gsap.from('#page-header-slogan', {opacity: 0, duration: 1, y: -50, delay: 0.8}) // header
+
+// ------------------------- GALLERY SECTION -------------------------
+const timeline = (target, trigger) => {
+    let tl = gsap.timeline({ // generating a timeline
+        scrollTrigger: {
+            trigger: trigger,
+        }
+    });
+
+    tl.from(target, {
+        y: 50, 
+        opacity: 0, 
+        duration: 2
+    })
+}
+
+timeline("#content-text-1", "#content-text-1");
+timeline("#content-text-2", "#content-text-2");
+timeline("#content-text-3", "#content-text-3");
+timeline("#content-text-4", "#content-text-4");
